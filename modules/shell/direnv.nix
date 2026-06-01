91d327fff5277b5f99134618a559c189e7c6d4da
Dave Verstrate <daverstrate@gmail.com>
2026-06-01 22:55:33 +0200

    fix: move direnv to global settings

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -5,19 +5,20 @@
       nixos = {
         programs.direnv = {
           enable = true;
+          settings.global.hide_env_diff = true;
         };
       };
-      hjem =
-        { pkgs, ... }:
-        {
-          xdg.config.files = {
-            "direnv/direnv.toml" = {
-              generator = (pkgs.formats.toml { }).generate "direnv.toml";
-              value = {
-                global.hide_env_diff = true;
-              };
-            };
-          };
-        };
+      # hjem =
+      #   { pkgs, ... }:
+      #   {
+      #     xdg.config.files = {
+      #       "direnv/direnv.toml" = {
+      #         generator = (pkgs.formats.toml { }).generate "direnv.toml";
+      #         value = {
+      #           global.hide_env_diff = true;
+      #         };
+      #       };
+      #     };
+      #   };
     };
 }
