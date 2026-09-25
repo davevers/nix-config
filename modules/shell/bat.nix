abadcc3eed520a33ca302dc05859d06f8f341023
Dave Verstrate <daverstrate@gmail.com>
2026-09-25 14:42:57 +0200

    change theme to evergarden

--- modules/shell/bat.nix
+++ modules/shell/bat.nix
@@ -10,8 +10,14 @@
     provides.to-users = {
       hjem = {
         xdg.config.files = {
-          "bat/config".text = "--theme=\"Rose-Pine-Dawn\"";
+          "bat/config".text = ''
+            --theme-dark="Evergarden Fall"
+            --theme-light="Evergarden Summer"
+            --theme=auto
+          '';
           "bat/themes/Rose-Pine-Dawn.tmTheme".source = "${self}/dots/bat/Rose-Pine-Dawn.tmTheme";
+          "bat/themes/evergarden-fall.tmTheme".source = "${self}/dots/bat/evergarden-fall.tmTheme";
+          "bat/themes/evergarden-summer.tmTheme".source = "${self}/dots/bat/evergarden-summer.tmTheme";
         };
       };
     };
